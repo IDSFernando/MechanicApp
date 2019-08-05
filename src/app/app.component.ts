310ef3d0c37c824caf37defffe37f65961181804
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-08-04 21:47:01 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 import { AccountPage } from './account/account.page';
 import { RESTService } from './rest.service';
-import { Router } from '@angular/router'
+import { Router, NavigationExtras } from '@angular/router'
 
 interface userData {
   email: string,
@@ -87,7 +87,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.backgroundColorByHexString('#ffffff');
+      this.statusBar.styleDefault();
       this.splashScreen.hide();
       this.screen.lock(this.screen.ORIENTATIONS.PORTRAIT)
       const tok = localStorage.getItem('auth_token')
@@ -112,6 +112,7 @@ export class AppComponent {
    */
   async getAllServices()
   {
+
     //Limpiar antes de hacer un push
     this.appPages = [
       {
@@ -203,7 +204,7 @@ export class AppComponent {
       header: "Mechanicapp",
       message: text,
       buttons: ['Ok'],
-      translucent: false
+      translucent: true
     });
     return await alert.present();
   }
@@ -223,8 +224,6 @@ export class AppComponent {
     return str;
   }
 
-
-
   async getIconFromKeyword(keyword)
   {
     let result = ''
@@ -236,5 +235,21 @@ export class AppComponent {
       }
     })
     return result
+  }
+
+  /**
+   * Reciclar la vista de búsqueda
+   *
+   * @param   {[String]}  servicio  servicio de búsqueda
+   */
+  goToThis(servicio)
+  {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        "busco": ''+servicio
+      }
+    }
+    
+    this.router.navigate(["busqueda"], navigationExtras)
   }
 }
